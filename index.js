145383086a4468c2cfc906f5f0e3f91f411affcf
domifar <135108965+domifar@users.noreply.github.com>
2024-03-24 22:20:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const session = require('express-session')
 const app = express()
-const port = 3003
+const port = 80
 
 app.use(express.static('public'))
 app.use(express.json())
